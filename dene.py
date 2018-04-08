5eedbea300be442297b548cd5d9f2b041c140051
Ubuntu <msenel@gmail.com>
2018-04-08 18:40:27 +0000

    VUI: Capstone project started
    Problem with model 0.

---
+++ dene.py
@@ -0,0 +1,4 @@
+from data_generator import vis_train_features
+
+# extract label and audio features for a single training example
+vis_text, vis_raw_audio, vis_mfcc_feature, vis_spectrogram_feature, vis_audio_path = vis_train_features()
